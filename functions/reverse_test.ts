28ba048006c2454f7ed77e840f91ff4f5afabb5c
Horea Porutiu <hporutiu@slack-corp.com>
2022-04-14 11:27:30 -0700

    Delete reverse_test.ts

--- functions/reverse_test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
-import ReverseString from "./reverse.ts";
-
-Deno.test("Reverse string function test", async () => {
-  const inputs = { stringToReverse: "foo" };
-  const { outputs } = await ReverseString({ inputs, env: {} });
-  assertEquals(outputs?.reverseString, "oof");
-});
